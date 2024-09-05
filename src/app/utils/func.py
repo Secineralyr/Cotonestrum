69c84c63d998efb13a34cfd8685d2e6e80d8df19
Komasan5364 <61271838+Komasan5364@users.noreply.github.com>
2024-09-06 01:14:48 +0900

    fix: Fix a bug when licence is null

--- src/app/utils/func.py
+++ src/app/utils/func.py
@@ -3,7 +3,7 @@ import re
 # returns list of tuple.
 # each tuple express (is_url, text).
 def detect_url(text):
-    t = text
+    t = text if text is not None else ''
     tl = []
     url_pattern = r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
     for _ in range(10): # url detection limit
